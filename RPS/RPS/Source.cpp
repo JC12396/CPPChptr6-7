d1a6f8e200e9bfeed6bb246b585a11407b4787bb
Joseph Chalupa <grammerking113@gmail.com>
2018-09-27 10:22:47 -0500

    UPLOAD

--- RPS/RPS/Source.cpp
+++ RPS/RPS/Source.cpp
@@ -18,37 +18,37 @@ int main()
 
 		if (choice == random)
 		{
-			cout << "You lost";
+			cout << "You lost" << endl;
 		}
 		else if (choice == 1 && random == 2)
 		{
-			cout << "You lost";
+			cout << "You lost" << endl;
 		}
 		else if (choice == 1 && random == 3)
 		{
-			cout << "You Won";
+			cout << "You Won" << endl;
 		}
 		else if (choice == 2 && random == 1)
 		{
-			cout << "You Won";
+			cout << "You Won" << endl;
 		}
 		else if (choice == 2 && random == 3)
 		{
-			cout << "You lost";
+			cout << "You lost" << endl;
 		}
 		else if (choice == 3 && random == 2)
 		{
-			cout << "You Won";
+			cout << "You Won" << endl;
 		}
 		else if (choice == 3 && random == 1)
 		{
-			cout << "You lost";
+			cout << "You lost" << endl;
 		}
 		else
 		{
-			cout << "INVALID";
+			cout << "INVALID" << endl;
 		}
-		cout << "Again?\n1- Yes\n2 - No";
+		cout << "Again?\n1- Yes\n2 - No" << endl;
 		cin >> again;
 
 	}while (again != 2);
--- USDto/USDto/Source.cpp
+++ USDto/USDto/Source.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
 {
+	double americanCurrency;
+	double britishPound;
+	double mexicanPeso;
+	double japaneseYen;
+	double canada;
+	double euro;
+	double rupee;
+	double rand;
 
 
+	cout << "How much USD are you converting?";
+	cin >> americanCurrency;
 
+	britishPound = americanCurrency * .6318;
+	mexicanPeso = americanCurrency * 12.8863;
+	canada = americanCurrency * .9813;
+	euro = americanCurrency * .757;
+	rupee = americanCurrency * 52.53;
+	rand = americanCurrency * 7.7522;
+	japaneseYen = americanCurrency * 82.34;
 
+	cout << americanCurrency << " USD is converted to " << britishPound << " British Pounds\n" << mexicanPeso << " Mexican Peso's\n" << japaneseYen << " Japanese Yen\n" << euro << " Euros\n" << rupee << " Rupees\n" << rand << " Rand\n" << canada << " Canada\n" << endl;
 
+	system("pause");
 
+	return 0;
 }
